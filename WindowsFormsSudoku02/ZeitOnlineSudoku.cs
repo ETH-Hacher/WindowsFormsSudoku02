876c20b6d8a344815267c8dea375e27ba39d45b1
Ahmed Ibrahim <aibrahim@lobo.ag>
2023-12-22 11:13:59 +0100

    with all 5 levels from Zeit.de Sudoku

---
+++ WindowsFormsSudoku02/ZeitOnlineSudoku.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsSudoku02
+{
+    internal class ZeitOnlineSudoku
+    {
+        public int id { get; set; }
+        public string game { get; set; }
+        public string solve { get; set; }
+    }
+}
